fb5c99154ba662d2ad8704db91e37d17c98435b4
Junior R Joseph <93812419+Lastkingjayjay@users.noreply.github.com>
2022-07-02 00:16:58 -0400

    javascript fundamentals

---
+++ java.js
@@ -0,0 +1,254 @@
+//document.write({name:'junior'})
+// alert({name:'junior'})
+// cons/ole.log({name:'junior'})
+
+// console.log('hello world');
+// console.log('hello people');
+// console.log('hello my friend peter');
+// console.log('hello susan');
+// let name ='junior joseph';
+// let address, zip, state;
+// address = '101 main street';
+// zip = '1596';
+// name ='tuna'
+// state = 'Philadelphia'
+// console.log(address, name, zip, state)
+
+// var value = 'some value'
+// const lastName = 'smith'
+// console.log(value)
+// console.log(lastName)
+
+// const name = 'john'
+// const lastName = 'roundtree'
+// let fullName = name +" "+ lastName;
+
+// console.log('hell there your full name is :' + fullName)
+
+// const website = 'github'
+// const url = 'https://www.' + website + '.com'
+// console.log(url)
+
+//Numbers
+// Loosley Tyepd = dont delcare type
+
+// const number = 34
+// // let pants = 2.456
+// // pants ='pants are greate '
+// const number2 =2.456
+// const number3 ="2.456"
+
+// const add = number + number2
+// const sub = number - number2
+// const mult = number * number2
+// const div = number / number2
+
+// console.log(add)
+// console.log(sub)
+// console.log(mult)
+// console.log(div)
+
+// console.log(number)
+// console.log(number2)
+// console.log(number3)
+
+// Numbers
+
+// +=, -=, *=, ++, --, %
+// Modulus (&)  operator returns the reaminder after integer divison
+
+// let number = 40
+// number += 5
+// number += 5
+// number += 5
+// number += 5
+// number += 5
+// number -= 5
+// number--
+// number--
+// number++
+
+// console.log(number)
+
+// const slices = 10
+// const children = 4
+// const amount = slices % children
+
+// const random = 4 + 5 + 6 + 10 * 10
+// const random2 =( 4 + 6 + 10)*10
+
+// console.log(random)
+// console.log(random2)
+
+// const value = name - lastName
+// console.log(value)
+
+// let number3 = 10
+// let number4 = 23
+//  number4 = 'pants'
+// const result2 = number3 + number4
+
+// console.log(result2)
+
+// Data Types - 7 total
+
+// Primitive - String, Number, boolean, Null, Undefined, Symbol
+// Object - Arrays, Functions, Objects
+
+// typeof - operator (typeof variable) (typeof value)
+
+// String
+// const text = 'some text'
+// // Number
+// const number = 45
+// // boolean
+// let value1 = true
+// let value2 = false
+// // Null
+// const result =null
+// // undefined
+// let name;
+// // Symbol(ES6)
+
+// console.log(typeof text)
+// console.log(typeof true)
+// console.log(typeof number)
+// console.log(typeof value2)
+// console.log(typeof value1)
+// console.log(typeof result)
+// console.log(typeof  name);
+
+// Arrays, Functions and Objects
+// Arrays -[], 0 index based
+
+// const friend1 = "anna";
+// const friend2 = "anna";
+// const friend3 = "anna";
+// const friend4 = "anna";
+
+// const friends = ["john", "peter", "bob", "susy", 45, undefined, null];
+
+// let bestFriend = friends[2];
+// friends[4] = "anna";
+// console.log(friends);
+// console.log(friends[4]);
+// console.log(bestFriend);
+
+// console.log(friends[6]);
+
+//Functions - declare, invoke
+
+// function hello(params) {
+//   //logic
+//   console.log("Hello There Bob");
+//   console.log("Hello There anna");
+//   console.log("Hello There Susy");
+// }
+// hello();
+// // console. log('Hello There Bob');
+// // console. log('Hello There john');
+// // console. log('Hello There Susy');
+// // // come code here....
+// hello();
+// // console. log('Hello There Bob');
+// // console. log('Hello There Anna');
+// // console. log('Hello There Susy');
+// // // come code here....
+// hello();
+// // console. log('Hello There Bob');
+// // console. log('Hello There Anna');
+// // console. log('Hello There Susy');
+// // // come code here....
+
+// Arrays, Functions and Objects
+
+// parans - when declare/define
+//placeholders, local vars
+// argunents - when invoke/call/run
+// use vars/values, multiple params, undefined
+// return
+// default undefined, shortcuts, ignores after
+
+// function hello(params) {
+//   //logic
+//   console.log("Hello There Bob");
+// }
+// function hello2(params) {
+//   //logic
+
+//   console.log("Hello There anna");
+// }
+// function hello3(params) {
+//   //logic
+
+//   console.log("Hello There Susy");
+// }
+
+// hello();
+// // greet bob
+// hello2();
+// //greet anna
+// hello3();
+// //greet susy
+// const bob = "bob";
+// const anna = "anna";
+// const susy = "susy";
+// const mike = "mike";
+
+// function greet(name, second, third, fouth) {
+//   console.log(second);
+//   console.log(third);
+//   console.log(fouth);
+//   console.log("hello there " + name);
+// }
+
+// // greet bob
+// greet("4");
+// //greet anna
+// greet(anna, bob, susy, mike);
+// //greet susy
+// greet("susy");
+
+// return
+// default undefined, shortcuts, ignores after
+// 1 inch 2.54cm
+// const wallheight = 80;
+// function calculate(value) {
+//   //   const newValue = value * 2.54;
+
+//   //   return newValue;
+//   return value * 2.54;
+// }
+
+// const width = calculate(100);
+// const height = calculate(wallheight);
+
+// const dimensons = [width, height];
+// console.log(dimensons);
+
+// Arrays, Functions and Objects
+
+// expressions - another way define a function
+
+// create a variable, assign to FUNCTION (not value), use var
+// diff - hoisting, use - arrow func, libraries,
+
+// function definition/declaration
+// function addValues(num1, num2) {
+//   return num1 + num2;
+// }
+// const firstvalue = addValues(3, 4);
+// const secondValue = addValues(12, 34);
+// // function expression
+// const add = function (num1, num2) {
+//   return num1 + num2;
+// };
+// // const thirdValue = add(5, 6);
+// const values = [firstvalue, secondValue, add(5, 6)];
+// console.log(values);
+
+// const multiply = (num1, num2) => num1 * num2
+
+// Arrays, Functions and Objects
+// Objects - key/value pairs methods
+// dot notation
